72ba03ccff1f3b6d3f218595ba2d583f65212d0f
Rafael Schoenardie <byschneider@gmail.com>
2017-03-27 21:20:00 -0300

    make it better

--- index.js
+++ index.js
@@ -9,13 +9,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 5000;
-const HOST = process.env.HOST;
-
-console.log("=> " + HOST);
 
 app.use(router.BASE_PATH, router.router);
 
-//app.listen(PORT, HOST);
 app.listen(PORT);
 
-console.log(`[${new Date().toLocaleString()}] - Server at ${HOST}:${PORT}`);
+console.log(`[${new Date().toLocaleString()}] - Server at ${PORT}`);
