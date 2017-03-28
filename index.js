5dd65ec1a00ed210078c06625a603b7567e76a0b
Rafael Schoenardie <byschneider@gmail.com>
2017-03-27 21:14:15 -0300

    make it better

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ const router = require('./server/router');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || '127.0.0.1';
+const PORT = process.env.PORT || 5000;
+//const HOST = process.env.HOST || '127.0.0.1';
 
 app.use(router.BASE_PATH, router.router);
 
+//app.listen(PORT, HOST);
 app.listen(PORT, HOST);
 
 console.log(`[${new Date().toLocaleString()}] - Server at ${HOST}:${PORT}`);
