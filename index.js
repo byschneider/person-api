0e09700508581e9319a285870f32270c2994bf28
Rafael Schoenardie <byschneider@gmail.com>
2017-03-31 14:47:44 -0300

    make it melhor

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const router = require('./server/router');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.use(router.BASE_PATH, router.router);
